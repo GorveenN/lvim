73fcbdd719253ac4772d067da623da0671e6ce12
Piotr Karpiński <gorveenn@gmail.com>
2022-02-06 20:26:01 +0100

    Minor fixes

--- config.lua
+++ config.lua
@@ -16,10 +16,10 @@ lvim.builtin.project.active = false
 lvim.builtin.terminal.active = false
 lvim.builtin.nvimtree.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 1
+lvim.builtin.nvimtree.setup.disable_netrw = false
 
--- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = { "rust", "yaml", "python", "cpp", "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.highlight.enable = true
 
 require("user.plugins").config()
 require("user.remaps").config()
