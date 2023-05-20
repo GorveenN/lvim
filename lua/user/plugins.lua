8e34752c48743bffee4b6a59c31fff694022211a
Piotr Karpiński <gorveenn@gmail.com>
2023-05-20 21:55:19 +0200

    Updates for LunarVim 1.3.0.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -6,23 +6,12 @@ M.config = function()
     { "folke/tokyonight.nvim" },
     {
       "folke/trouble.nvim",
-      requires = "kyazdani42/nvim-web-devicons",
+      dependecies = "nvim-tree/nvim-web-devicons",
       config = function()
         require("trouble").setup {
-          -- -- settings without a patched font or icons
-          -- {
-          --     fold_open = "v", -- icon used for open folds
-          --     fold_closed = ">", -- icon used for closed folds
-          --     indent_lines = false, -- add an indent guide below the fold icons
-          --     signs = {
-          --         -- icons / text used for a diagnostic
-          --         error = "error",
-          --         warning = "warn",
-          --         hint = "hint",
-          --         information = "info"
-          --     },
-          --     use_lsp_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
-          -- }
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
         }
       end,
     },
@@ -53,7 +42,7 @@ M.config = function()
     },
     {
       "iamcco/markdown-preview.nvim",
-      run = "cd app && npm install",
+      build = "cd app && npm install",
       ft = "markdown",
       config = function()
         vim.g.mkdp_auto_start = 1
@@ -83,7 +72,7 @@ M.config = function()
       },
       ft = { "fugitive" },
     },
-    { "tpope/vim-rhubarb", requires = { "tpope/vim-fugitive" } },
+    { "tpope/vim-rhubarb", dependecies = { "tpope/vim-fugitive" } },
     -- {
     --   "folke/zen-mode.nvim",
     --   config = function()
